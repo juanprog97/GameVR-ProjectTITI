26dbda5de5aea4ec5f93e4e928270d004d19b0d6
juanprog97 <juancard190@gmail.com>
2022-02-09 18:15:00 -0500

    Making animator

---
+++ Assets/Script/ContinuosMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+public class ContinuosMovement : MonoBehaviour
+{
+    public float speed = 1;
+    public XRNode inputSource;
+    public Vector2 inputAxis;
+    public float gravity = -9.81f;
+    private float fallingSpeed;
+    private XRRig rig;
+    public float additionalHeight = 0.2f;
+    public LayerMask groundLayer;
+    private CharacterController character;
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+
+
+    }
+    private void FixedUpdate()
+    {
+        CapsuleFollowHeadset();
+
+        Quaternion headyaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+        Vector3 direction = headyaw *  new Vector3(inputAxis.x, 0, inputAxis.y);
+        character.Move(direction* Time.fixedDeltaTime*speed);
+        //gravity
+        bool isGround = CheckIfGround();
+        if (isGround)
+            fallingSpeed = 0;
+        else
+            fallingSpeed += gravity * Time.fixedDeltaTime;
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+
+        
+                   
+
+
+
+    }
+
+    void CapsuleFollowHeadset()
+    {
+        character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
+    }
+
+    bool CheckIfGround()
+    {
+        //tell if Ground
+        Vector3 rayStart = transform.TransformPoint(character.center);
+        float rayLength = character.center.y + 0.01f;
+        bool hasHit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+
+        return hasHit;
+    }
+}
--- Assets/Script/HandPresence.cs
+++ Assets/Script/HandPresence.cs
@@ -6,15 +6,24 @@ using UnityEngine.XR;
 public class HandPresence : MonoBehaviour
 {
     public InputDeviceCharacteristics controllerCharacteristic;
-    public bool showController = true;
+    private bool showController = true;
     private InputDevice targetDevice;
     public List<GameObject> controllerPrefabs;
     private GameObject spawnedController;
     private GameObject spawnedHandModel;
     public GameObject handModelPrefab;
+    private Animator animatorHand;
     // Start is called before the first frame update
     void Start()
     {
+        tryInitialize();
+
+
+    }
+
+    void tryInitialize()
+    {
+
         List<InputDevice> devices = new List<InputDevice>();
 
         InputDevices.GetDevicesWithCharacteristics(controllerCharacteristic, devices);
@@ -23,7 +32,7 @@ public class HandPresence : MonoBehaviour
 
         }
         Debug.Log(devices.Count);
-        if(devices.Count > 0)
+        if (devices.Count > 0)
         {
             targetDevice = devices[0];
             GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
@@ -34,40 +43,73 @@ public class HandPresence : MonoBehaviour
             else
             {
                 Debug.Log("Did not found Model corresponding");
-                spawnedController = Instantiate(controllerPrefabs[0] , transform);
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
             }
             spawnedHandModel = Instantiate(handModelPrefab, transform);
+            animatorHand = spawnedHandModel.GetComponent<Animator>();
 
         }
-        
+
+    }
+
+    void UpdateAnimatorHand()
+    {
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue)){
+            animatorHand.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            animatorHand.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            animatorHand.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            animatorHand.SetFloat("Grip", 0);
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-       /* if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)           That Code is for see state of event of control
-            Debug.Log("Pressing Button Primary Button");
 
-        
-        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
-            Debug.Log("Trigger Pressed Button" + triggerValue);
+        /* if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)           That Code is for see state of event of control
+             Debug.Log("Pressing Button Primary Button");
 
-        
-        if(targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue !=  Vector2.zero)
-            Debug.Log("Primary TouchPad" + primary2DAxisValue);*/ 
 
-        if (showController)
+         if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) && triggerValue > 0.1f)
+             Debug.Log("Trigger Pressed Button" + triggerValue);
+
+
+         if(targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue !=  Vector2.zero)
+             Debug.Log("Primary TouchPad" + primary2DAxisValue);*/
+
+        if (!targetDevice.isValid)
         {
-            spawnedHandModel.SetActive(false);
-            spawnedController.SetActive(true);        
+            tryInitialize();
         }
         else
         {
-            spawnedHandModel.SetActive(true);
-            spawnedController.SetActive(false);
+            if (showController)
+            {
+                spawnedHandModel.SetActive(false);
+                spawnedController.SetActive(true);
+            }
+            else
+            {
+                spawnedHandModel.SetActive(true);
+                spawnedController.SetActive(false);
+                UpdateAnimatorHand();
 
+            }
         }
+
+       
 
 
     }
