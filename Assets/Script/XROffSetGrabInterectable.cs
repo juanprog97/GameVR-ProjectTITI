13b41b97c4c61ccbc22783e00cb884c5586701d1
juanprog97 <juancard190@gmail.com>
2022-02-10 18:20:03 -0500

    Add Script fundamentals

---
+++ Assets/Script/XROffSetGrabInterectable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+public class XROffSetGrabInterectable : XRGrabInteractable
+{
+    // Start is called before the first frame update
+    private Vector3 initialAttachLocalPos;
+    private Quaternion initialAttachLocalRot;
+    void Start()
+    {
+        if (!attachTransform)
+        {
+            GameObject grab = new GameObject("Grab Pivot");
+            grab.transform.SetParent(transform, false);
+            attachTransform = grab.transform;
+        }
+        initialAttachLocalPos = attachTransform.localPosition;
+        initialAttachLocalRot = attachTransform.localRotation;
+
+
+    }
+
+    // Update is called once per frame
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        if (interactor is XRDirectInteractor)
+        {
+            attachTransform.position = interactor.transform.position;
+            attachTransform.rotation = interactor.transform.rotation;
+        }
+        else
+        {
+            attachTransform.localPosition = initialAttachLocalPos;
+            attachTransform.localRotation = initialAttachLocalRot;
+        }
+        base.OnSelectEntered(interactor);
+    }
+  
+}
